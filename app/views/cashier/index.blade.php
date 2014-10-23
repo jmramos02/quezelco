ce8dbab5e067cb9665b745bd2184d7e0a9df76b6
jaotors <turingan.joshua@gmail.com>
2014-10-23 19:41:22 +0800

    merging ability

--- app/views/cashier/index.blade.php
+++ app/views/cashier/index.blade.php
@@ -11,9 +11,13 @@
 		<div class="col-md-12">
 			{{Form::open(array('url' =>'cashier/payment/search-oebr', 'method' => 'get'))}}
 			<div class="error">{{$errors->first('oebr')}}</div>
-			<div class="col-md-12">
+			<div class="col-md-6">
 				<h6>OEBR</h6>
 				{{Form::text('oebr','',array('class' => 'form-control'))}}
+			</div>
+			<div class="col-md-6">
+				<h6>Senior Citizen ?</h6>
+				{{Form::checkbox('is_senior','',false,array('class' => 'form-control'))}}
 			</div>
 			<div class="col-md-12">	
 				<input type="submit" class="btn btn-primary btn-lg" value="Search">
